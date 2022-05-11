a34bea8c50ec0402ceb78b582d806551cbddd38d
Marcus <marcus.chong@trentglobal.edu.sg>
2022-05-11 13:16:18 +0000

    added dropdown for listing page

--- cakeeey/src/pages/Product.js
+++ cakeeey/src/pages/Product.js
@@ -5,16 +5,38 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 export default function Product() {
+
   let { cake_id } = useParams();
   const arrSize = [];
   const arrIngredient = [];
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
-  const [size, setSize] = useState([]);
+
+  const [sizeg, setSize] = useState(
+    [{
+      id: "1231231",
+      size: "345345ghjfgh3534",
+    }]
+  );
+
+  const [sizeStore, setSizeStore] = useState(
+    [{
+      id: "",
+      size: "",
+    }]
+  );
+
   const [description, setDescription] = useState("");
   const [ingredient, setIngredient] = useState([]);
   const [image, setImage] = useState("");
+
+  const updateFormField = (e) => {
+    setSizeStore({
+      ...sizeg,
+      [e.target.name]: e.target.value
+    });
+  };
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -22,22 +44,23 @@ export default function Product() {
         "https://cakeeey.herokuapp.com/api/products/cakes/" + cake_id
       );
 
-      //get back array of possible cake sizes for the particular cake
-      let haveProduct = response.data[0].products;
-      for (let i of haveProduct) {
-        if (!arrSize.includes(i.cakesize.size)) {
-          arrSize.push(i.cakesize.size);
-        }
-      }
-      setSize(arrSize);
 
-      let haveIngredient = response.data[0].ingredients;
-      for (let i of haveIngredient) {
-        if (!arrIngredient.includes(i.name)) {
-          arrIngredient.push(i.name);
+      //if name has been loaded
+      
+        let haveProduct = response.data[0].products;
+        for (let i of haveProduct) {
+          arrSize.push(i.cakesize);
         }
+        setSize(arrSize);
+
+        let haveIngredient = response.data[0].ingredients;
+        for (let i of haveIngredient) {
+          if (!arrIngredient.includes(i.name)) {
+            arrIngredient.push(i.name);
+          }
+        
+        // setSize(arrIngredient);
       }
-      setSize(arrIngredient);
 
       //if exist for price we just take the first
       if (response.data[0].products[0]) {
@@ -49,7 +72,7 @@ export default function Product() {
       setImage(response.data[0].image_url);
     };
     fetchPost();
-  }, [cake_id]);
+  }, []);
 
   return (
     <React.Fragment>
@@ -64,10 +87,27 @@ export default function Product() {
 
         {/* details of the item */}
         <div className="col-5">
-            <div className="FontMain" style={{fontSize:"32px"}}>{name}</div>
-            <div className="FontMain" style={{fontSize:"24px"}}>
-            {parseFloat(price/100).toFixed(2)}
-            </div>
+          <div className="FontMain" style={{ fontSize: "32px" }}>
+            {name}
+          </div>
+          <div className="FontMain" style={{ fontSize: "24px" }}>
+            {parseFloat(price / 100).toFixed(2)}
+          </div>
+          <div className="p-2">
+            <label className="drop-down-label-width">Select Cake Size</label>
+            <select
+              value={sizeg.size}
+              name="id"
+              onChange={updateFormField}
+              className="drop-down-width"
+            >
+              {sizeg && sizeg.map((p) => (
+                <option value={p.id}>{p.size}</option>
+              ))}
+            </select>
+          </div>
+          ;{/* description */}
+          <div>{description}</div>
         </div>
       </div>
     </React.Fragment>
